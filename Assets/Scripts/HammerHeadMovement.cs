75435fb18f87bd9a50284c3ddce85b84691cd834
James Lillicrap <jamesplillicrap@gmail.com>
2019-03-29 16:37:56 -0400

    3.8.1
    Implemented HammerHead damage

--- Assets/Scripts/HammerHeadMovement.cs
+++ Assets/Scripts/HammerHeadMovement.cs
@@ -8,12 +8,41 @@ public class HammerHeadMovement : MonoBehaviour
     public float velocityx;         //float value that reads the x velocity of the Hammer Head
     public float velocityy;         //float value that reads the y velocity of the Hammer Head
     public float cooldownspeed;     //float value that reads the speed threshold where the Hammer Head can be launched at
+    public bool damageseq;          //determines if the ship is in the damage sequence
+    public int hp;                  //the amount of health that the hammerhead has
     public Sprite[] images;         //sprite array stores all the sprites that the Hammer Head will display
     public Object crosshairo;       //crosshairo or crosshair object is the crosshair prefab
+    public Object destruction;      //particle effect on destruction
     private SpriteRenderer image;   //image is the SpriteRenderer component attached to the gameobject
     private Rigidbody2D rb;         //rb is the Rigidbody2D componenet attached to the GameObject
     private GameObject crosshair;   //crosshair is the crosshair GameObject in the scene
     private Collider2D col;         //col is the Collider2D object attached to the GameObject
+
+    public IEnumerator Damage()
+    {
+        damageseq = true;
+        hp -= 1;
+        Instantiate(destruction, transform.position, transform.rotation);
+        image.enabled = false;
+        col.enabled = false;
+        yield return new WaitForSeconds(1);
+        GameObject asteroidspawner = GameObject.FindGameObjectWithTag("AsteroidSpawner");
+        asteroidspawner.GetComponent<AsteroidSpawner>().enabled = false;
+        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        image.enabled = true;
+        image.color = new Color(255.0f, 255.0f, 255.0f, .5f);
+        GameObject[] asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
+        for(int i=0; i<asteroids.Length; ++i)
+        {
+            Destroy(asteroids[i]);
+        }
+        yield return new WaitForSeconds(3);
+        rb.velocity = Vector2.zero;
+        asteroidspawner.GetComponent<AsteroidSpawner>().enabled = true;
+        col.enabled = true;
+        damageseq = false;
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +57,14 @@ public class HammerHeadMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Sqrt(Mathf.Pow(rb.velocity.x,2)+Mathf.Pow(rb.velocity.y,2)) < cooldownspeed)
+        
+        if (Mathf.Sqrt(Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.y, 2)) < cooldownspeed)
         {
             image.sprite = images[0];
-            image.color = Color.white;
+            if (damageseq == false)
+            {
+                image.color = Color.white;
+            }
             col.isTrigger = false;
             float distancex = crosshair.transform.position.x - transform.position.x;
             float distancey = crosshair.transform.position.y - transform.position.y;
@@ -44,9 +77,9 @@ public class HammerHeadMovement : MonoBehaviour
             {
                 transform.eulerAngles = new Vector3(0.0f, 0.0f, angle + 180);
             }
-            if (Input.GetButtonDown("Fire1"))
+            if ((Input.GetButtonDown("Fire1")) && (damageseq == false))
             {
-                rb.AddForce(transform.up * speed * Mathf.Sqrt(distancex*distancex+distancey*distancey));
+                rb.AddForce(transform.up * speed * Mathf.Sqrt(distancex * distancex + distancey * distancey));
             }
         }
         else
@@ -57,7 +90,11 @@ public class HammerHeadMovement : MonoBehaviour
         }
         velocityx = rb.velocity.x;
         velocityy = rb.velocity.y;
-    }
+        if(rb.velocity.magnitude < .01f)
+        {
+            rb.velocity = Vector2.zero;
+        }
+}
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Asteroid")
@@ -67,6 +104,13 @@ public class HammerHeadMovement : MonoBehaviour
             {
                 split.Explode();
             }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Asteroid")
+        {
+            StartCoroutine(Damage());
         }
     }
 }
---
+++ Assets/Scripts/LifeBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBehavior : MonoBehaviour
+{
+    public GameObject[] children;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        children = new GameObject[gameObject.transform.childCount];
+        for(int i=0; i < children.Length; ++i)
+        {
+            children[i] = transform.GetChild(i).gameObject;
+            children[i].GetComponent<Image>().sprite = player.GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.name == "HammerHead")
+        {
+            HammerHeadMovement hbehavior = player.GetComponent<HammerHeadMovement>();
+            if(hbehavior.hp == 0)
+            {
+                children[0].GetComponent<Image>().enabled = false;
+                children[1].GetComponent<Image>().enabled = false;
+                children[2].GetComponent<Image>().enabled = false;
+            }else if(hbehavior.hp == 1)
+            {
+                children[0].GetComponent<Image>().enabled = false;
+                children[1].GetComponent<Image>().enabled = false;
+                children[2].GetComponent<Image>().enabled = true;
+            }else if (hbehavior.hp == 2)
+            {
+                children[0].GetComponent<Image>().enabled = false;
+                children[1].GetComponent<Image>().enabled = true;
+                children[2].GetComponent<Image>().enabled = true;
+            }else if (hbehavior.hp == 3)
+            {
+                children[0].GetComponent<Image>().enabled = true;
+                children[1].GetComponent<Image>().enabled = true;
+                children[2].GetComponent<Image>().enabled = true;
+            }
+        }
+    }
+}
